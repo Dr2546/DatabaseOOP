732f129269b3be34eb1352d5a61b3018e5d6e79c
Dr2546 <danryv46@gmail.com>
2022-04-18 04:00:42 +0700

    add img command

--- src/DB.java
+++ src/DB.java
@@ -1,27 +1,30 @@
-import java.util.HashMap;
-import java.sql.*;  
+import java.sql.*;
+import java.io.*;
+import java.util.HashMap;  
+
 public class DB
 {
     
     char type;
-    Connection pat;
+    Connection req;
     
     public boolean init(){
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            this.pat=DriverManager.getConnection(  
+            this.req=DriverManager.getConnection(  
             "jdbc:mysql://db4free.net:3306/dandogdb","dandog","daniel2546");  
             return true;
         }catch(Exception e)
         {
+           // System.out.println(e);
             return false;
         }
     }
 
     public boolean insert(String username,String password,String email){
         try{
-            Statement stmt=this.pat.createStatement();  
-            String insert = "'"+username+"','"+password+"','member','"+email+"'";
+            Statement stmt=this.req.createStatement();  
+            String insert = "'"+username+"','"+password+"','1','"+email+"'";
             int rs = stmt.executeUpdate("insert into people(username,password,role,email) values("+insert+")");
             if(rs > 0)
             {
@@ -38,7 +41,7 @@ public class DB
     
     public boolean delete(String username){
         try{
-            Statement stmt = this.pat.createStatement();
+            Statement stmt = this.req.createStatement();
             String condition = "username='"+username+"'";
             int rs = stmt.executeUpdate("delete from people where "+condition);
             if(rs > 0){
@@ -56,8 +59,9 @@ public class DB
     public HashMap<String,String> find(String username){
         HashMap<String,String> data = new HashMap<>();
         try{
-            Statement stmt = this.pat.createStatement();
+            Statement stmt = this.req.createStatement();
             ResultSet rs = stmt.executeQuery("select * from people where username='" + username + "'");
+            rs.next();
             data.put("username",rs.getString(1));
             data.put("password",rs.getString(2));
             data.put("role",rs.getString(3));
@@ -65,42 +69,71 @@ public class DB
             return data;
         }
         catch(Exception e){
-            return data;
+            System.out.println(e);
+            return null;
         }
     }
 
+    public boolean insertImg(String path,String imgname){
+        try{
+            File file = new File(path);
+            FileInputStream fis=new FileInputStream(file);
+            PreparedStatement ps=this.req.prepareStatement("insert into img (name,image) values(?,?)");
+            ps.setString(1,imgname);
+            ps.setBinaryStream(2,fis,(int)file.length());
+            ps.executeUpdate();
+ 
+            ps.close();
+            fis.close();
+            return true;
+        }catch(Exception e){
+            System.out.println(e);
+            return false;
+        }
+    }
 
-
-    /*public static boolean login(String username,String password){
-        boolean res = false;
-        try{  
-            Class.forName("com.mysql.cj.jdbc.Driver");  
-            Connection con=DriverManager.getConnection(  
-            "jdbc:mysql://db4free.net:3306/dandogdb","dandog","daniel2546");  
-            //here sonoo is database name, root is username and password  
-            Statement stmt=con.createStatement();  
-            ResultSet rs = stmt.executeQuery("select * from people where username='" + username + "'");
-            if(!rs.next())
-            {
-                System.out.println("No user found");
-            }
-            else{
-                //System.out.println("t");
-                //System.out.println(rs.getString(1)+"  "+rs.getString(2));
-                if(!password.equals(rs.getString(2)))
+    public boolean getImg(String path,String imgname,String imgtype){
+        try{
+            File file;
+            if(path.equals("none"))
                 {
-                    System.out.println("Password not match");
+                    file = new File("System.getProperty(\"user.home\")" + File.separator + "\"testimg\"");
+                    file.mkdirs();
                 }
-                else{
-                    System.out.println("Login success");
-                    res = true;
-                }
+            else 
+                file = new File(path);
+            FileOutputStream fos=new FileOutputStream(new File(file, imgname+"."+imgtype));
+            byte b[];
+            Blob blob;
+            String sqlcommand = "select * from img where name=\"" + imgname + "\"";
+            PreparedStatement ps=this.req.prepareStatement(sqlcommand);
+            ResultSet rs=ps.executeQuery();
+            while(rs.next()){
+                blob=rs.getBlob("image");
+                b=blob.getBytes(1,(int)blob.length());
+                fos.write(b);
             }
-            con.close();
-            return res;  
-            }catch(Exception e){ 
-                System.out.println(e);
-                return false;
-            }
-    }*/
+            ps.close();
+            fos.close();
+            
+
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+    public Connection getCon()
+    {
+        return this.req;
+    }
+
+    public DB() {
+        this('u');
+    }
+
+    public DB(char type) {
+        this.type = type;
+    }
+
 }  
--- src/test.java
+++ src/test.java
@@ -1,7 +1,11 @@
+//import java.io.*;
+//import java.sql.*;
+
 public class test {
     public static void main(String[] args) {
         DB db = new DB();
         System.out.println(db.init());
-        System.out.println(db.insert("daniel", "daniel", "daniel@dan.com"));
+        System.out.println(db.insertImg("C:\\Users\\Lenovo\\Dan\\Dan\\kmitl\\y1s2\\OOP\\project\\img\\hituesday.jpg", "hituesday"));
+        System.out.println(db.getImg("C:\\Users\\Lenovo\\testimg", "hituesday","jpg")); 
     }
 }
